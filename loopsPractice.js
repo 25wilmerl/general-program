63b52ab89d10f953ea876f9b78540556953dcc57
Wilmer <25WilmerL@badinhs.org>
2025-02-21 13:43:24 -0500

    still working on loopsPractice

---
+++ loopsPractice.js
@@ -0,0 +1,20 @@
+// This is the first task
+for (let i = 0; i < 23; i++) {
+         if( i % 2 == 0){
+             continue;
+         }
+    console.log(i);
+        }
+
+
+//This is the second task
+
+let i = 0
+while (i < 3){
+    console.log(i);
+    i++;
+}
+
+    //     for (let i = 0; i < 3; i++) { 
+    //         alert( `number ${i}!` ); 
+    // }
