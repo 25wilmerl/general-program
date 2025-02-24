59cdfb54303f694c5e9c461f4b434ef7c9b9e4bb
Wilmer <25WilmerL@badinhs.org>
2025-02-24 13:45:55 -0500

    worked on loopsPratice.js

--- loopsPractice.js
+++ loopsPractice.js
@@ -1,22 +1,28 @@
 // This is the first problem
 
 
-for (let i = 0; i < 23; i++) {
-         if( i % 2 == 0){
-             continue;
-         }
-    console.log(i);
-        }
+// for (let i = 0; i < 23; i++) {
+//          if( i % 2 == 0){
+//              continue;
+//          }
+//     console.log(i);
+//         }
 
 
-//This is the second problem
+// //This is the second problem
 
-let i = 0
-while (i < 3){
-    console.log(i);
-    i++;
-}
+// let i = 0
+// while (i < 3){
+//     console.log(i);
+//     i++;
+// }
 
 //This is the third problem
-prompt("Please type in a number greater than 100");
-console.log()
+input = 0;
+while(input <= 100){
+    input = prompt("Please type in a number greater than 100");
+}
+
+//This is the fourth problem
+
+function sumInput()
